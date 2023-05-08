c5025f980cdf06262a50c553710f0abcb42c95d8
Stephan Schorer <stephan.schorer@outlook.de>
2023-05-08 22:56:56 +0200

    created script uploaded data to redis

--- src/tools/downloadImage/downloadImage.py
+++ src/tools/downloadImage/downloadImage.py
@@ -3,6 +3,7 @@ import os
 import zipfile
 import pyminizip as zip
 from tqdm import tqdm
+import time
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -20,9 +21,9 @@ original_dir = os.getcwd()
 
 # Download all objects that start with '10'
 # can be disabled by removing the prefix parameter in line 21
-object_prefix = '10'
+object_prefix = '1030'
 minio_bucket = 'museum'
-objects = list(client.list_objects(minio_bucket, prefix=object_prefix)) #
+objects = list(client.list_objects(minio_bucket)) #, prefix=object_prefix
 
 def checkPathExistence():
     try:
@@ -37,21 +38,54 @@ def checkPathExistence():
 
 checkPathExistence()
 
+bigjpg_objects = [obj for obj in objects if "_0.JPG" in obj.object_name]
+smalljpg_objects = [obj for obj in objects if "_0.jpg" in obj.object_name]
+
+objects=smalljpg_objects
+
 total_items = len(objects)
+print('Downloading Items: ',total_items)
+
 for obj in tqdm(objects, total=total_items, bar_format="{percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt}"):
-    # Download the object data
-    filenamejpg = obj.object_name.lower()
-    filenamezip = filenamejpg.replace('.jpg', '.zip')
+    if "_0.jpg" in obj.object_name:
+        # Download the object data
+        filenamejpg = obj.object_name.lower()
+        filenamezip = filenamejpg.replace('.jpg', '.zip')
 
-    if not os.path.isfile(image_folder+filenamejpg):
-        client.fget_object(minio_bucket, filenamejpg, file_path=uncompress_folder+filenamezip)
-        zip.uncompress(uncompress_folder+filenamezip, os.getenv('ENCRYPTION_KEY'), uncompress_folder, 1)
-        os.remove(filenamezip)
+        if not os.path.isfile(image_folder+filenamejpg):
+            try:
+                client.fget_object(minio_bucket, filenamejpg, file_path=uncompress_folder+filenamezip)
+            except Exception as e:
+                print(e)
 
-        os.chdir(original_dir)
-        extracted_file_name = os.listdir(uncompress_folder)[0]
-        
-        os.rename(uncompress_folder+extracted_file_name, image_folder+filenamejpg)
-    else:
-        print()
-        print(f'{filenamejpg} already exists - skipping')
+            zip.uncompress(uncompress_folder+filenamezip, os.getenv('ENCRYPTION_KEY'), uncompress_folder, 1)
+            os.remove(filenamezip)
+
+            os.chdir(original_dir)
+            extracted_file_name = os.listdir(uncompress_folder)[0]
+            
+            os.rename(uncompress_folder+extracted_file_name, image_folder+filenamejpg)
+        else:
+            print()
+            print(f'{filenamejpg} already exists - skipping')
+    if "_0.JPG" in obj.object_name:
+        # Download the object data
+        filenamejpg = obj.object_name
+        filenamezip = filenamejpg.replace('.JPG', '.zip')
+    
+        if not os.path.isfile(image_folder+filenamejpg):
+            try:
+                client.fget_object(minio_bucket, filenamejpg, file_path=uncompress_folder+filenamezip)
+            except Exception as e:
+                print(e)
+    
+            zip.uncompress(uncompress_folder+filenamezip, os.getenv('ENCRYPTION_KEY'), uncompress_folder, 1)
+            os.remove(filenamezip)
+    
+            os.chdir(original_dir)
+            extracted_file_name = os.listdir(uncompress_folder)[0]
+            
+            os.rename(uncompress_folder+extracted_file_name, image_folder+filenamejpg.replace('.JPG', '.jpg'))
+        else:
+            print()
+            print(f'{filenamejpg} already exists - skipping')
--- src/tools/imageClassification/processImages.py
+++ src/tools/imageClassification/processImages.py
@@ -1,6 +1,7 @@
 import torch
 from torchvision import models, transforms
 from PIL import Image
+Image.LOAD_TRUNCATED_IMAGES = True
 import pandas as pd
 import os
 import numpy as np
@@ -33,21 +34,40 @@ from dotenv import load_dotenv
 load_dotenv()
 
 redis_client = redis.StrictRedis(host=os.getenv('redis_host'), port=os.getenv('redis_port'), password=os.getenv('redis_password'))
-#redis_client2 = redis.StrictRedis(host=os.getenv('redis_host_manu'), port=os.getenv('redis_port_manu'), password=os.getenv('redis_password_manu'))
-image_folder = os.getenv('image_folder')
+redis_client_manu = redis.StrictRedis(host=os.getenv('redis_host_manu'), port=os.getenv('redis_port_manu'), password=os.getenv('redis_password_manu'))
+redis_client_prod = redis.StrictRedis(host=os.getenv('redis_host_prod'), port=os.getenv('redis_port_prod'), password=os.getenv('redis_password_prod'))
 
 #Model imports
  #mobilenet_v2 = models.mobilenet_v2(weights="MobileNet_V2_Weights.DEFAULT")
  #mobilenet_v3_small = models.mobilenet_v3_small(weights="MobileNet_V3_Small_Weights.DEFAULT")
-mobilenet_v3_large = models.mobilenet_v3_large(weights="MobileNet_V3_Large_Weights.DEFAULT")
+ #mobilenet_v3_large = models.mobilenet_v3_large(weights="MobileNet_V3_Large_Weights.DEFAULT")
  #alexnet = models.alexnet(weights="AlexNet_Weights.DEFAULT")
  #efficientnet = models.efficientnet_b0(weights="EfficientNet_B0_Weights.DEFAULT")
  #vgg = torch.hub.load('pytorch/vision:v0.10.0', 'vgg11', pretrained=True)
- #ResNet50 = models.resnet50(weights="ResNet50_Weights.DEFAULT")
+ResNet50 = models.resnet50(weights="ResNet50_Weights.DEFAULT")
 
-#Define the model to use in the code below:
-model=mobilenet_v3_large
 
+
+"""
+# # # # # # # # # # # # # # # # # # # # # # # # # #
+# Variable Deklaration for the rest of the code:  #
+# # # # # # # # # # # # # # # # # # # # # # # # # #
+"""
+# Which Model should be used?
+model = ResNet50
+# Which Redis DB should be used?
+redis_client = redis_client_prod
+# Where are the iamges stored?
+image_folder = os.getenv('image_folder')
+
+
+"""
+Creates a directory at the path specified by the 'image_folder' environment variable if it does not already exist.
+If the directory already exists, nothing happens. If an error occurs when creating the directory, the program exits.
+
+Returns:
+    None
+"""
 def checkPathExistence():
     try:
         if not os.path.exists(os.getenv('image_folder')):
@@ -56,6 +76,17 @@ def checkPathExistence():
         print(e)
         exit()
 
+
+"""
+Create a tensor from an image file using the provided PyTorch model. The function takes in
+a path to an image file as input and returns a PyTorch tensor. The model used in this
+function should already be set to evaluation mode. The function uses PyTorch's transforms
+module to preprocess the image by resizing it to 256x256, cropping the center to 224x224,
+converting it to a tensor, and normalizing it using the ImageNet mean and standard deviation.
+The function then prepares the input tensor to be used as a batch, and applies the model to
+this batch either on CPU or GPU depending on availability. Finally, the function returns
+the output tensor.
+"""
 def createTensor(image_path):
     # Set model to evaulation mode
     model.eval()
@@ -87,10 +118,18 @@ def createTensor(image_path):
 
     return tensor
 
+
+"""
+Uploads a tensor to Redis as a JSON object with a vector field.
+:param tensor: A tensor to upload.
+:type tensor: Tensor
+:param objname: The name of the Redis object to store the tensor in.
+:type objname: str
+"""
 def uploadTensorToRedis(tensor,objname):
     #Store the vector in Redis as HASH
      #tensor_bytes = tensor.numpy().astype(np.float32).tobytes(order='C')
-     #redis_client.hset(objname, mapping={"vector_field": tensor_bytes})
+     #redis_client.hset(objname, mapping={"vector_field": tensor_bytes})    
 
     ###################################################################
     # WARNING !!!!!!!!!
@@ -99,26 +138,50 @@ def uploadTensorToRedis(tensor,objname):
     ###################################################################
 
     #Overwrite json in Redis DB
-    redis_client.json().set(objname, '$', {"vector": tensor[0].tolist()})
+     #redis_client.json().set(objname, '$', {"vector": tensor[0].tolist()})
 
     #Add Vector Data to JSON in Redis
-     #redis_client.json().set(objname, '$.vector', {"vector": tensor[0].tolist()})    
+    redis_client.json().set(redis_client.incr('MyKey'), '$.vector', {"vector": tensor[0].tolist()})
 
+
+"""
+Create a search index in RedisAI for the given index_name. The index is defined using a json schema and 
+has a single vector field with 1000 dimensions, using the L2 distance metric. 
+
+Args:
+    index_name (str): The name of the index to be created.
+
+Returns:
+    None
+"""
 def createIndex(index_name):
-    # Create index
     t0 = time.time()
-    redis_client.ft(index_name=index_name).create_index(
-            fields=(
-                VectorField(                    
-                    "$.vector", "FLAT", {"TYPE": "FLOAT32", "DIM": 1000, "DISTANCE_METRIC": "L2"}, as_name="vectorfield"
-                )
-            ),
-            definition=IndexDefinition(index_type=IndexType.JSON)
-        )
-    t1 = time.time()
-    total = t1-t0
-    print(f'Index created in {total} seconds')
+    try:
+        redis_client.ft(index_name=index_name).create_index(
+                fields=(
+                    VectorField(                    
+                        "$.Tensor", "FLAT", {"TYPE": "FLOAT32", "DIM": 1000, "DISTANCE_METRIC": "L2"}, as_name="vectorfield"
+                    )
+                ),
+                definition=IndexDefinition(index_type=IndexType.JSON)
+            )
+        t1 = time.time()
+        total = t1-t0
+        print(f'Index created in {total} seconds')
+    except Exception as e:
+        print(e)
 
+
+"""
+Performs a KNN search on an index using a given search tensor and index name.
+
+Args:
+    search_tensor: A tensor that will be used for the KNN search.
+    index_name: The name of the index to perform the search on.
+
+Returns:
+    The results of the KNN search.
+"""
 def searchKNN(search_tensor, index_name):
     # vector_test for Similarity Search
     search_tensor_bytes = search_tensor[0].numpy().astype(np.float32).tobytes(order='C')
@@ -137,48 +200,45 @@ def searchKNN(search_tensor, index_name):
 
     return result
 
-def processImages(image_path):    
+
+"""
+Processes images in a given directory and uploads their tensors to Redis. 
+
+Args:
+    image_path (str): A string representing the path to the directory containing the images.
+
+Returns:
+    None
+"""
+def processImages(image_folder):    
     checkPathExistence()
-    print(f'Processing images in {image_path} - can take a while...')    
-    images = os.listdir(image_path)    
+    print(f'Processing images in {image_folder} - can take a while...')    
+    images = os.listdir(image_folder)    
     if len(images) == 0:
-        print(f'No images found in {image_path}')
+        print(f'No images found in {image_folder}')
         exit()
 
     for image in tqdm(images, bar_format="{percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt}"):
-        if not redis_client.exists(image):
-            uploadTensorToRedis(createTensor(image_path+image), image)
-        else:
-            print()
-            print(f'Tensor for {image} already exists - skipping')
+        try:
+            if not redis_client.exists(image):
+                uploadTensorToRedis(createTensor(image_folder+image), image)
+            else:
+                print()
+                print(f'Tensor for {image} already exists - skipping')
+        except Exception as e:
+            print(image)
+            print(e)
+            continue
 
 
-################
-# Test Section #
-################
-
-#Upload one Image Tensor to Redis:
- #uploadTensorToRedis(tensor1,'1402')
-
-#Create Image Tensors of image_folder and Upload to Redis:
- #processImages(image_folder)
-
-#Create index for KNN Search
- #createIndex('myindex1')
-
-#KNN Search for given searchtensor
-searchtensor = createTensor('C:/Users/steph/.cache/downloadImage/image_folder/1007_0.jpg')
-result = searchKNN(searchtensor, "myindex1")
-#Print the results of the searchKNN function
-for doc in result.docs:  
-    json_data = json.loads(doc.json)
-    print('objname: ',doc.id,'| score: ',doc.__vectorfield_score)
-    #print('vector in json: ',json_data['vector'])
-
-
-# Compare different models and there scores
- #modellist = [ResNet50, mobilenet_v2, mobilenet_v3_small, mobilenet_v3_large, alexnet, efficientnet, vgg]
+"""
+Iterates over a list of models and performs image processing, indexing, and searching
+using each model. Prints the model name and search results for each model. Flushes
+Redis at the end of each iteration. Returns nothing.
+"""
 def evauluatModels():
+    #modellist = [ResNet50, mobilenet_v2, mobilenet_v3_small, mobilenet_v3_large, alexnet, efficientnet, vgg]
+
     # iterate over the models
     for model in modellist:
         t0 = time.time()
@@ -194,6 +254,77 @@ def evauluatModels():
         t1 = time.time()
         total = t1-t0
         print(f'Index created in {total} seconds')
+
+
+"""
+Uploads old data from a folder to Redis.
+
+Returns:
+    None
+"""
+def uploadOldDataToRedis(): 
+    images = os.listdir(image_folder)    
+    if len(images) == 0:
+        print(f'No images found in {image_folder}')
+        exit()
+
+    f = open('./src/tools/imageClassification/oldDataSet.json')
+    myjson = json.load(f)
+    t0 = time.time()
+
+    for image in tqdm(images, bar_format="{percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt}"):
+        try:            
+            tensor = createTensor(image_folder+image)
+            myjsonentry = None
+            for entry in myjson:                           
+                if entry['ObjektId'] == int(image.split('_')[0]):                        
+                    entry['Tensor'] = tensor[0].tolist()
+                    myjsonentry = entry
+                    break
+            
+            redis_client.json().set(redis_client.incr('MyKey'), '$', myjsonentry)  
+        except Exception as e:
+            print(image)
+            print(e)
+            continue
+    
+    t1 = time.time()
+    total = t1-t0
+    print(f'Upload took: {total} seconds')
+
+    #for item in jsonList:
+    #    uploadTensorToRedis(createTensor(item), item)
+
+
+
+#uploadOldDataToRedis()
+
+
+
+################
+# Test Section #
+################
+
+#Upload one Image Tensor to Redis:
+ #uploadTensorToRedis(tensor1,'1402')
+
+#Create Image Tensors of image_folder and Upload to Redis:
+ #processImages(image_folder)
+
+#Create index for KNN Search
+createIndex('vectorIndex')
+
+#KNN Search for given searchtensor
+def testKNNsearch():
+    searchtensor = createTensor('C:/Users/steph/.cache/downloadImage/image_folder/5_0.jpg')
+    result = searchKNN(searchtensor, "vectorIndex")
+    #Print the results of the searchKNN function
+    for doc in result.docs:  
+        json_data = json.loads(doc.json)
+        print('objname: ',doc.id,'\t|\tscore: ',doc.__vectorfield_score)
+        #print('vector in json: ',json_data['vector'])
+
+#testKNNsearch()
 
 #evauluatModels()
 

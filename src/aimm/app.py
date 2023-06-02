213a6dde4b8ccf47d9c7d6a330c80f6d2d6a78a0
cwenzl <christian.wenzl@hs-augsburg.de>
2023-06-03 00:34:24 +0200

    Test_Commit

--- src/aimm/app.py
+++ src/aimm/app.py
@@ -13,6 +13,8 @@ from wtforms.validators import data_required
 
 load_dotenv()  # take environment variables from .env.
 
+#Ich mag Kekse
+
 IMG_FOLDER = './static/img'
 UPLOAD_FOLDER = 'ImgUpload/'
 app = Flask(__name__)
@@ -23,7 +25,6 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 REDIS_CLIENT = redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"),
                            password=os.getenv("REDIS_PASSWORD"))
-
 
 # form to enter the object data
 class ObjectForm(FlaskForm):

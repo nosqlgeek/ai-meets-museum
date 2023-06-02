4172e7f33e04c09b9ea229c52da741cedc9521fd
Stephan Schorer <stephan.schorer@outlook.de>
2023-06-02 15:44:25 +0200

    added createIndex

--- src/aimm/database.py
+++ src/aimm/database.py
@@ -83,6 +83,62 @@ def uploadObjectToRedis(json_data):
 
 
 """
+Creates a new index in Redisearch with the specified index_name.
+Args:
+    index_name (str): The name of the index to create.
+    recreate (bool): If set to True, drops the index if it already exists and creates a new one.
+Returns:
+    None
+"""
+def createIndex(index_name, recreate=False):
+    def createIndexFunction():
+        redis_client.ft(index_name=index_name).create_index(
+            fields=(
+                TextField(
+                    "$.Bezeichnung", as_name="Bezeichnung"
+                ),
+                TextField(
+                    "$.InventarNr", as_name="InventarNr"
+                ),
+                TextField(
+                    "$.Material", as_name="Material"
+                ),
+                TextField(
+                    "$.Beschreibung", as_name="Beschreibung"
+                ),
+                TextField(
+                    "$.TrachslerNr", as_name="TrachslerNr"
+                ),
+                VectorField(                    
+                    "$.Tensor", "FLAT", {"TYPE": "FLOAT32", "DIM": 1000, "DISTANCE_METRIC": "L2"}, as_name="vectorfield"
+                )
+            ),
+            definition=IndexDefinition(index_type=IndexType.JSON)
+        )
+    
+    t0 = time.time()
+    try:
+        if recreate:
+            try:
+                redis_client.ft(index_name=index_name).dropindex()
+            except Exception as e:
+                print('Index does not exist')
+            finally:
+                createIndexFunction()                
+        else:
+            if not redis_client.exists(index_name):
+                createIndexFunction()
+            else:
+                print(f'Index {index_name} already exists')
+                exit()
+        t1 = time.time()
+        total = t1-t0
+        print(f'Index created in {total} seconds')
+    except Exception as e:
+        print(e)
+
+
+"""
 Performs a KNN search on an index using a given search tensor and index name.
 
 Args:
@@ -161,7 +217,6 @@ def getFullTextSearchCount(searchKeywords, index_name):
             )
             
     return result.total
-
 
 
 """

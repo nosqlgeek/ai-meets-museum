e89570c314c9153590691645e2c02b151c65a6ad
cwenzl <christian.wenzl@hs-augsburg.de>
2023-06-06 12:19:00 +0200

    Checking if commit to the main branch is done additionally

--- src/aimm/app.py
+++ src/aimm/app.py
@@ -13,7 +13,7 @@ from wtforms.validators import data_required
 
 load_dotenv()  # take environment variables from .env.
 
-#test commit
+#test commit 02
 
 IMG_FOLDER = './static/img'
 UPLOAD_FOLDER = 'ImgUpload/'
